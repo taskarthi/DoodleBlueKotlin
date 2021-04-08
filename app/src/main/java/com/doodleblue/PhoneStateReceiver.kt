73c50831a8bb68ce76846112c3bf85867abb05d2
karthi <taskarthi@gmail.com>
2021-04-08 19:42:59 +0530

    popup

--- app/src/main/java/com/doodleblue/PhoneStateReceiver.kt
+++ app/src/main/java/com/doodleblue/PhoneStateReceiver.kt
@@ -6,9 +6,8 @@ import android.content.Intent
 import android.telephony.PhoneNumberUtils
 import android.telephony.TelephonyManager
 import android.util.Log
-import android.view.WindowManager
 import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
+
 
 
 class PhoneStateReceiver : BroadcastReceiver() {
